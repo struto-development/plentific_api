16e736947b86d2370e16f78f00a0ff3f813ce265
Archie Makuwa <archie.makuwa@impact.com>
2019-12-12 11:35:16 +0200

    Cleaned up project and split classes into four, Authentication, Deals, ErrorReporting and StoredData

---
+++ classes/updateDeal.php
@@ -0,0 +1,36 @@
+<?php 
+
+class updateDeal extends Authenticate {
+
+    public function update_deal_data() {
+
+
+
+
+
+
+
+
+        /*
+        $endPoint = "https://api.hubapi.com/deals/v1/deal";
+		
+		$accesToken = $this->getAccessToken();
+
+		if (!isset($accesToken)) {
+			return array('error' => 'true', 'msg' => 'Error getting access token.');
+		}
+		
+		$contact = new Contact();
+		$contactID = $contact->getContactVID();
+
+		if (!empty($contactID[0])) {
+			$clientEmail = $contactID[0];
+			$venueEmail = $contactID[1];
+		} else {
+			return array('error' => 'true', 'msg' => 'Create Booking Error 2: Please try again.'); //format of error msg to be decided
+		}
+        */
+
+    }
+
+}
